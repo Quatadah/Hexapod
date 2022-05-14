131026df2c180534c0d552bf498066d3952c8815
DKHISSI Youness <Youness.Dkhissi@bordeaux-inp.fr>
2022-05-14 21:21:59 +0200

    Co-authored-by: Ghofrane473 <Ghofrane473@users.noreply.github.com>

--- main.py
+++ main.py
@@ -10,6 +10,35 @@ import sys
 from signal import signal, SIGINT
 import traceback
 import os
+from kinematics import * 
+
+
+class P:
+    def __init__(
+        self,
+        z=-0.06,
+    ):
+        self.z = z
+        # Angle between the X axis of the leg and the X axis of the robot for each leg
+        self.legAngles = LEG_ANGLES
+        # Initial leg positions in the coordinates of each leg.
+        self.initLeg = []  # INIT_LEG_POSITIONS
+        self.initLeg.append([0.170, 0])
+        self.initLeg.append([0.170, 0])
+        self.initLeg.append([0.170, 0])
+        self.initLeg.append([0.170, 0])
+        self.initLeg.append([0.170, 0])
+        self.initLeg.append([0.170, 0])
+
+        # Motor re-united by joint name for the simulation
+        self.legs = {}
+        self.legs[1] = ["1", "2", "3"]
+        self.legs[6] = ["4", "5", "6"]
+        self.legs[5] = ["7", "8", "9"]
+        self.legs[2] = ["10", "11", "12"]
+        self.legs[3] = ["13", "14", "15"]
+        self.legs[4] = ["16", "17", "18"]
+
 
 # import display
 
@@ -37,7 +66,6 @@ def main():
         sys.exit()
         # Brutal exit
         # os._exit(1)
-
     # Tell Python to run the shutdown() function when SIGINT is recieved
     signal(SIGINT, shutdown)
     try:
@@ -54,14 +82,33 @@ def main():
         )
 
         print("Setting initial position")
-
+        params1 = P()
         # TODO create this function instead:
-        setPositionToRobot(0, 0, 0, robot, params)
-        
-        robot.smooth_tick_read_and_write(3, verbose=False)
-        print("Init position reached")
-        time.sleep(2)
-        print("Closing")
+        setPositionToRobot(10, 0, 0, robot, params)
+        while True:
+            targets={}
+            print("avant  robot.legs.items():\n ",  robot.legs.items())
+            print("****************************************************")
+            walk(params.freq, params1, targets, math.pi/4, 0.1, 0.1)
+            for k, v in robot.legs.items():
+            # Setting 0 s the goal position for each motor (such sadness! When we could be using the glorious inverse kinematics!)
+                print("avant affectation v est \n", len(v) )
+                print("le v[i] est \n", v[0].goal_position)
+             
+            i = 1
+            for k, v in robot.legs.items():
+            # Setting 0 s the goal position for each motor (such sadness! When we could be using the glorious inverse kinematics!)
+                print("targets est \n", targets)
+                v[0].goal_position = targets[str(i)]
+                v[1].goal_position = targets[str(i+1)]
+                v[2].goal_position = targets[str(i+2)]
+                i+=3
+
+            print("avant  targets.items(): \n",  targets.items())
+            robot.smooth_tick_read_and_write(3, verbose=False)
+            print("Init position reached")
+            time.sleep(2)
+            print("Closing")
     except Exception as e:
         track = traceback.format_exc()
         print(track)
